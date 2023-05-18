333f63999eba37e584b418abfe5ad5f35a7a17bf
Bartlomiej <bartlomiejkrason@outlook.com>
2023-05-18 22:51:09 +0200

    ball

--- ball.py
+++ ball.py
@@ -1,29 +1,36 @@
 import cv2
-import threading
 import numpy as np
 
-class Ball(threading.Thread):
+class Ball:
     def __init__(self, x_pos, y_pos, speed, radius):
-        super(Ball, self).__init__()
-        # self.ball_thread = ball_thread
         self.x_pos = x_pos
         self.y_pos = y_pos
         self.speed = speed
         self.radius = radius
+        self.window_width = None
+        self.window_height = None
 
-    def move(self, speed):
-        self.x_pos += speed[0]
-        self.y_pos += speed[1]
+    def set_window_size(self, width, height):
+        self.window_width = width
+        self.window_height = height
 
-    def run(self, speed, width, height):
-        # while self.running:
-        self.move(speed)
-        self.WindowDetection(width, height)
+    def move(self):
+        self.x_pos += self.speed[0]
+        self.y_pos += self.speed[1]
 
-    def WindowDetection(self, window_width, window_height):
-        # Wykrywanie odbić od krawędzi okna
-        if self.x_pos + self.radius >= window_width or self.x_pos - self.radius <= 0:
+        if self.x_pos <= self.radius or self.x_pos >= self.window_width - self.radius:
+            self.speed[0] *= -1
+
+        if self.y_pos <= self.radius or self.y_pos >= self.window_height - self.radius:
             self.speed[1] *= -1
 
-        if self.y_pos + self.radius >= window_height or self.y_pos - self.radius <= 0:
+    def detect_collision(self, edges):
+        edges_gray = cv2.cvtColor(edges, cv2.COLOR_BGR2GRAY)
+        ret, edges_binary = cv2.threshold(edges_gray, 1, 255, cv2.THRESH_BINARY)
+        mask = np.zeros_like(edges_binary)
+        cv2.circle(mask, (self.x_pos, self.y_pos), self.radius, 255, -1)
+        intersection = cv2.bitwise_and(mask, edges_binary)
+        if cv2.countNonZero(intersection) > 0:
             self.speed[0] *= -1
+            self.speed[1] *= -1
+
--- main.py
+++ main.py
@@ -10,24 +10,25 @@ def main():
     edge_detection_thread.start()
 
     speed = [1, 1]
-    ball = Ball(20, 20, speed, 10)
+    ball = Ball(100, 50, speed, 20)
+
     while True:
         edges = edge_detection_thread.get_edges()
 
         if edges is not None:
             frame = camera_thread.get_frame()
-            window_height, window_width = frame.shape[:2]
-            if frame is not None:
-                edges_resized = cv2.resize(edges, (frame.shape[1], frame.shape[0]))
-                frame_with_edges = cv2.addWeighted(frame, 0.5, edges_resized, 0.5, 0)
-                cv2.imshow('Edges', frame_with_edges)
-                # cv2.circle(frame, (frame.shape[1]//2, frame.shape[0]//2), 10, (0,255,0), thickness=-1)
-                cv2.circle(frame, (ball.x_pos, ball.y_pos), ball.radius, (0,255,0), thickness=-1)
-                # ball.move(speed)
-                # ball.WindowDetection(window_height, window_width)
-                ball.run(speed, window_width, window_height)
-                print(speed)
+            window_height, window_width = frame.shape[:2]  # Ustaw rozmiary okna na rozmiary ramki
+            ball.set_window_size(window_width, window_height)  # Ustaw rozmiary okna w obiekcie piłki
 
+            edges_resized = cv2.resize(edges, (frame.shape[1], frame.shape[0]))
+            frame_with_edges = cv2.addWeighted(frame, 0.5, edges_resized, 0.5, 0)
+            cv2.imshow('Edges', frame_with_edges)
+            
+            ball.move()  # Porusz piłką
+            ball.detect_collision(edges)  # Wykrywaj kolizje z krawędziami
+            
+            cv2.circle(frame, (ball.x_pos, ball.y_pos), ball.radius, (0,255,0), thickness=-1)
+            
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
